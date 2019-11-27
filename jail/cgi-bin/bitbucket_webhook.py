7c3761f1f1e470a730027281f2dc96fbd93c5143
Maxime Piraux <maxime.piraux@uclouvain.be>
2019-11-27 09:49:36 +0100

    Fix an odd Apache CGI suEXEC bug by placing the tmp_dir in the script_dir

--- jail/cgi-bin/bitbucket_webhook.py
+++ jail/cgi-bin/bitbucket_webhook.py
@@ -5,9 +5,11 @@ import sys
 import json
 
 import cgitb
-import tempfile
+import uuid
 
-cgitb.enable(display=0, logdir="/tmp")
+print()
+print()
+cgitb.enable(logdir="/tmp")
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 base_dir = os.path.dirname(os.path.dirname(script_dir))
@@ -15,6 +17,6 @@ base_dir = os.path.dirname(os.path.dirname(script_dir))
 body = json.loads(sys.stdin.read(int(os.environ["CONTENT_LENGTH"])))
 for change in body['push']['changes']:
     for commit in change['commits']:
-        tdir = tempfile.mkdtemp(prefix="pquic_repo_")
+        tdir = os.path.join(script_dir, 'pquic_repo_%s' % (str(uuid.uuid4())[:8]))
         subprocess.Popen(['/usr/bin/bash', os.path.join(base_dir, 'test_commit.sh'), tdir, commit['hash']])#, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, close_fds=True)
         break  # Only test the head commit
