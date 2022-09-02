2277ae346ea6c5c44a306dbce675c989b450f058
François Michel <francois.michel@uclouvain.be>
2022-09-02 10:36:27 +0200

    add prepare_video.sh to setup the video use-case

---
+++ prepare_video.sh
@@ -0,0 +1,6 @@
+mkdir -p $NS3_PATH/files-0
+mkdir -p $NS3_PATH/files-1
+cp /pquic-ns3-dce/myscripts/video-with-losses/bbb_720p_* $NS3_PATH/files-0/
+cp /pquic-ns3-dce/myscripts/video-with-losses/bbb_720p_* $NS3_PATH/files-1/
+cp /pquic-ns3-dce/myscripts/video-with-losses/tixeo_* $NS3_PATH/files-0/
+cp /pquic-ns3-dce/myscripts/video-with-losses/tixeo_* $NS3_PATH/files-1/
