4e2ba1dfadc0ddf4d4ffb25e84bc66d69b250796
François Michel <francois.michel@uclouvain.be>
2022-09-13 11:57:51 +0200

    check arguments of reproduce_and_plot_results.sh

--- reproduce_and_plot_results.sh
+++ reproduce_and_plot_results.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+    echo "Usage: bash $0 flec_directory (the flec_directory is probably ../flec ?)"
+    exit -1
+fi
 
 docker build -t pquic-ns3-dce-base-20.04 -f Dockerfile-base-20.04 .
 docker build -t pquic-ns3-dce -f Dockerfile-20.04 .
@@ -7,4 +12,3 @@ docker build -t pquic-ns3-dce -f Dockerfile-20.04 .
 python3 reproduce_experiments.py $1
 
 bash plot_flec_graphs.sh
-
